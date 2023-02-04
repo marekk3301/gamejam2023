9e6d809c7822d464549f648737d7bbbf6ce49e27
Mariusz <83692077+Superbananax@users.noreply.github.com>
2023-02-04 10:26:10 +0100

    Center planet

---
+++ scenes/planet.gd
@@ -0,0 +1,18 @@
+extends Sprite
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var screen_size = get_viewport().size
+	self.position = Vector2(screen_size.x / 2, screen_size.y / 2)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
