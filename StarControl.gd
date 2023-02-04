304218186ffd95d0bb50a4dd8423188a6523d883
Kinga Marszałkowska <kinga.marszalkowska72@gmail.com>
2023-02-04 10:56:59 +0100

    aa

--- StarControl.gd
+++ StarControl.gd
@@ -1,8 +1,14 @@
 extends Control
 
+var start_game_label = Label.new()
+	
+# Called when the node enters the scene tree for the first time.
+func _on_ParentControl_resized():
+	start_game_label.set_position(Vector2(get_size().x / 2, get_size().y / 2))
+
 func _ready():
+	get_tree().get_root().connect("size_changed", self, "_on_ParentControl_resized")
 	# tworzy napis i dodaje go do sceny
-	var start_game_label = Label.new()
 	start_game_label.set_text("Start game")
 	start_game_label.set_position(Vector2(get_size().x / 2, get_size().y / 2))
 	add_child(start_game_label)
