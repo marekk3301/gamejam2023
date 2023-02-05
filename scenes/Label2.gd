11080963aa9d3a76670055e741656b51e13a577f
Mariusz <83692077+Superbananax@users.noreply.github.com>
2023-02-05 12:54:55 +0100

    Scoree fix

--- scenes/Label2.gd
+++ scenes/Label2.gd
@@ -8,6 +8,4 @@ func _process(delta):
 	var score = 0
 	GlobalVariables.increment_Score(0.009)
 	score =  int(GlobalVariables.get_Score())
-	if GlobalVariables.get_Seed_Count() > 5:
-		score += 100
 	text = str(score)
