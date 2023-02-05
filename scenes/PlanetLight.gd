49e5e42e939ba8119409c4b5c7102b40f6cab8c3
Mariusz <83692077+Superbananax@users.noreply.github.com>
2023-02-05 13:14:12 +0100

    fix light

--- scenes/PlanetLight.gd
+++ scenes/PlanetLight.gd
@@ -5,14 +5,7 @@ func _ready():
 	_on_ParentControl_resized()
 	get_tree().get_root().connect("size_changed", self, "_on_ParentControl_resized")
 func _process(delta):
-	if growing:
-		self.energy += 0.05		
-	else:
-		self.energy -= 0.05		
-
-	if self.energy >= 0.8 or self.energy <= 0.07:
-		growing = !growing
-
+	pass
 func _on_ParentControl_resized():
 	var screen_size = get_viewport().size
 	self.position = Vector2(screen_size.x / 2, screen_size.y / 2)
