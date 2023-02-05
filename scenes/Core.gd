f9c4478b41e8342d8be0c283fac2416065fdcec4
Mariusz <83692077+Superbananax@users.noreply.github.com>
2023-02-05 09:31:18 +0100

    asdasd

--- scenes/Core.gd
+++ scenes/Core.gd
@@ -4,7 +4,6 @@ extends AnimatedSprite
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var rng = RandomNumberGenerator.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,8 +13,6 @@ func _process(delta):
 	if GlobalVariables.get_cracksOnCoreCount() == 0:
 		$".".play("CoreIdle")
 		$".".rotate(delta*-0.04)
-		$".".position.x += rng.randi_range(2, 8) * delta * 0.1 # dodaje małą zmienną do pozycji X obiektu
-		$".".position.y += rng.randi_range(1, 8) * delta * 0.1 # dodaje małą zmienną do pozycji Y obiektu
 	elif GlobalVariables.get_cracksOnCoreCount() == 1:
 		$".".play("Corestage1")
 	elif GlobalVariables.get_cracksOnCoreCount() == 2:
